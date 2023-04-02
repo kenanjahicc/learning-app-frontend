845122034706d29ad47d637a854e95e1bf530605
Kenan Jahic <kenanjahicc@gmail.com>
2023-04-03 00:30:23 +0200

    Added Homepage with nav and footer as separate components

--- TeachMe/src/app/app.module.ts
+++ TeachMe/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
