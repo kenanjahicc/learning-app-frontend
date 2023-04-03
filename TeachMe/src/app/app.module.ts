2edf4ec85ba636982869a7a491a10f06b2677dec
Malik <malik.krijestorac@stu.ssst.edu.ba>
2023-04-03 21:34:38 +0200

    Added submit button

--- TeachMe/src/app/app.module.ts
+++ TeachMe/src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { ProfessorPageComponent } from './professor-profile-page/professor-page.component';
 import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     AppComponent,
     FeedbackComponent,
     ProfessorPageComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatInputModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatInputModule,
+        FormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
