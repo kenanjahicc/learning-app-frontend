3542e9559722d91f72e21901ae24c393f2e175a2
zatega <denijal.zatega@gmail.com>
2023-04-04 01:29:11 +0200

    Login and register added

--- TeachMe/src/app/app.module.ts
+++ TeachMe/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { HomeComponent } from './home/home.component';
@@ -15,6 +17,9 @@ import { WrongRouteComponent } from './wrong-route/wrong-route.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from "@angular/forms";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,15 +31,21 @@ import {FormsModule} from "@angular/forms";
     ProfilePageComponent,
     StudentProfilePageComponent,
     ProfessorProfilePageComponent,
-    WrongRouteComponent
+    WrongRouteComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatInputModule,
     FormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
