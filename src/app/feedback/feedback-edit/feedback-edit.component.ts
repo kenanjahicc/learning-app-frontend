3f251a964079079536245e3e45e75eb9eeedcbf7
Malik <malik.krijestorac@stu.ssst.edu.ba>
2023-06-10 17:39:08 +0200

    Implemented feedback update and delete functionalities

--- src/app/feedback/feedback-edit/feedback-edit.component.ts
+++ src/app/feedback/feedback-edit/feedback-edit.component.ts
@@ -4,7 +4,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Subscription} from 'rxjs';
 import {FeedbackService} from "../../services/feedback.service";
 import {Professor} from "../../models/professor.model";
-import {MatDialog} from "@angular/material/dialog";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -14,53 +14,65 @@ import {MatDialog} from "@angular/material/dialog";
 })
 export class FeedbackEditComponent implements OnInit, OnDestroy {
 
-  private feedback: Feedback[] | undefined;
+  public form!: FormGroup;
+  public options = [
+    {
+      label: 'Complaint',
+      color: '#a34',
+    },
+    {
+      label: 'Observation',
+      color: '#adeeaf',
+    },
+    {
+      label: 'Suggestion',
+      color: '#9bd8f0',
+    },
+  ];
 
+  private feedback!: Feedback;
+
+  private unsubscribe: Subscription | undefined;
   private professor!: Professor;
 
-  private unsubscirbe: Subscription | undefined;
-
   constructor(private activatedRoute: ActivatedRoute,
+              private feedbackService: FeedbackService,
               private router: Router,
-              private feedvackService: FeedbackService,
-              private dialog: MatDialog) {
+              private formBuilder: FormBuilder) {
   }
+
 
   ngOnInit(): void {
-    this.unsubscirbe = this.activatedRoute.data.subscribe((data) => {
-      this.feedback = data['feedbacks'];
+    this.unsubscribe = this.activatedRoute.data.subscribe((data) => {
+      this.feedback = data['feedback'];
       this.professor = data['professor'];
     })
+    this.form = this.formBuilder.group({
+      'title': [this.feedback?.title, Validators.required],
+      'content': [this.feedback?.content, Validators.required],
+      'professorId': [this.professor.id, Validators.required],
+    });
   }
 
-  // updateFeedback(feedback: Feedback): void {
-  //   this.feedvackService.updateFeedback(feedback).subscribe(() => {
-  //     this.navigateToFeedbacks();
-  //   })
-  // }
-
-
-
-
-
-  // deleteFeedback(): void {
-  //   const dialogRef = this.dialog.open(YesNoDialogComponent, {
-  //     data: {
-  //       title: 'Delete milestone',
-  //       text: 'Are you sure you want to delete this milestone?',
-  //     },
-  //   });
-  //
-  //   dialogRef.afterClosed().subscribe(result => {
-  //     if (result) {
-  //       this.feedvackService.deleteFeedback(this.feedback.id).subscribe(() => {
-  //         this.navigateToMilestones();
-  //       });
-  //     }
-  //   });
-  // }
 
   ngOnDestroy(): void {
-    this.unsubscirbe!.unsubscribe();
+    this.unsubscribe!.unsubscribe()
   }
+
+  submit(): void {
+    this.updateFeedback(this.form.value);
+  }
+
+  updateFeedback(feedback: Feedback): void {
+    this.feedbackService.updateFeedback(feedback, this.feedback.id!).subscribe(() => {
+      this.navigateToProfessor();
+    });
+  }
+
+  navigateToProfessor() {
+    this.router.navigate([
+      this.professor.id
+    ])
+  }
+
 }
